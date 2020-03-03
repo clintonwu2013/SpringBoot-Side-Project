ab25f481c76aba79d994037899aefe1068f04098
6503 <6503@TV6503NB.tradevanad.intranet.local>
2020-03-03 16:51:31 +0800

    註解jpa程式

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -13,16 +13,16 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(DemoApplication.class, args);
 		
-		CustomerService customerService = context.getBean(CustomerService.class); // 取得CustomerService的bean
-
-        CustomerBo bo1 = new CustomerBo();
-        bo1.setId(10001L);
-        bo1.setName("John");
-        bo1.setAddress("No. 7, Yixin St., ");
-        customerService.add(bo1); // 新增
-
-        CustomerBo bo2 = customerService.get(10001L); // 查詢
-        System.out.println(bo2); 
+//		CustomerService customerService = context.getBean(CustomerService.class); // 取得CustomerService的bean
+//
+//        CustomerBo bo1 = new CustomerBo();
+//        bo1.setId(10001L);
+//        bo1.setName("John");
+//        bo1.setAddress("No. 7, Yixin St., ");
+//        customerService.add(bo1); // 新增
+//
+//        CustomerBo bo2 = customerService.get(10001L); // 查詢
+//        System.out.println(bo2); 
 	}
 
 }
