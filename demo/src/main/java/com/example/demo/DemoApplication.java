2c8b98cdf464d6917aa1c9ecb362a6b9bd17c13e
6503 <6503@TV6503NB.tradevanad.intranet.local>
2020-03-03 16:47:06 +0800

    reverse the example for JPA

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -11,18 +11,18 @@ import com.example.demo.service.CustomerService;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(DemoApplication.class, args);
 		
-//		CustomerService customerService = context.getBean(CustomerService.class); // 取得CustomerService的bean
+		CustomerService customerService = context.getBean(CustomerService.class); // 取得CustomerService的bean
 
-//        CustomerBo bo1 = new CustomerBo();
-//        bo1.setId(10001L);
-//        bo1.setName("John");
-//        bo1.setAddress("No. 7, Yixin St., ");
-//        customerService.add(bo1); // 新增
-//
-//        CustomerBo bo2 = customerService.get(10001L); // 查詢
-//        System.out.println(bo2); 
+        CustomerBo bo1 = new CustomerBo();
+        bo1.setId(10001L);
+        bo1.setName("John");
+        bo1.setAddress("No. 7, Yixin St., ");
+        customerService.add(bo1); // 新增
+
+        CustomerBo bo2 = customerService.get(10001L); // 查詢
+        System.out.println(bo2); 
 	}
 
 }
